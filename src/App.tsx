916aae889be67b15f525b7377564d0879dbc6344
jovemdan <danilogdev@gmail.com>
2022-04-22 20:42:50 -0300

    feat: criando a modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
 import { Dashboard } from './components/Dashboard'
 import { Header } from './components/Header'
 import { GlobalStyle } from './styles/global'
+import Modal from 'react-modal'
+import { useState } from 'react'
 
+Modal.setAppElement('#root')
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false)
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true)
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false)
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+
+      <Modal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      >
+        <h2>Cadastrar transação</h2>
+      </Modal>
       <Dashboard />
       <GlobalStyle />
     </>
